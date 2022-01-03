e2e25011b203036284003e15d54027b5bc1f476e
Maksim Aisin <maksim.aisin@mail.ru>
2022-01-03 20:54:56 +0300

    feat: release number reverse functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let nArray = [...n.toString()].reverse();
+    let convertedArray = [];
+    nArray.forEach((value) => convertedArray.push(parseInt(value)));
+    return parseInt(convertedArray.join(""));
+};
